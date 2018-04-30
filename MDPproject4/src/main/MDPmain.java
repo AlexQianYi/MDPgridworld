228641fab5ea6862823bd0c8740f4ec39647f3d0
AlexQianYi <q563875735@gmail.com>
2018-04-30 00:15:03 -0400

    panel

--- MDPproject4/src/main/MDPmain.java
+++ MDPproject4/src/main/MDPmain.java
@@ -1,5 +1,19 @@
 package main;
 
 public class MDPmain {
-
+	
+	Grid_Board board;
+	int col_num;
+	int row_num;
+	
+	private void MDPmain() {
+		this.board = new Grid_board();
+		this.col_num = board.getCol();
+		this.row_num = board.getRow();
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
